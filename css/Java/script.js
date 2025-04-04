c21a368ac4bb36bf186a0d80abc4960288fba654
Lorris02 <110474346+b12lor@users.noreply.github.com>
2025-04-03 23:45:39 -0400

    Delete css directory

--- css/Java/script.js
+++
@@ -1,7 +0,0 @@
-const toggleHamburgerButton = document.querySelector(".hamburger-container");
-const dropDownMenu = document.querySelector(".dropdown-menu");
-
-function toggleHamburger() {
-  toggleHamburgerButton.classList.toggle("active");
-  dropDownMenu.classList.toggle("open");
-}
